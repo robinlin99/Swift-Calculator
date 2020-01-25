546298e1112f14ff0f8017a985f95eb43b124fc1
Robin (Zihao) Lin <robin.lin99@yahoo.com>
2020-01-25 17:04:52 -0500

    Second

--- Tutorial/ViewController.swift
+++ Tutorial/ViewController.swift
@@ -9,7 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var displayText:Double = 0;
+    var tracker:Double = 0;
+    
+    @IBOutlet weak var display: UILabel!
+    
+    @IBAction func numbers(_ sender: UIButton) {
+        if display.text == "0" {
+            display.font = display.font.withSize(80)
+            display.text = String(sender.tag-1)
+        }
+        else {
+            display.text = display.text! + String(sender.tag-1)
+            displayText = Double(display.text!)!
+            if (display.text!).count > 5 {
+                display.font = display.font.withSize(50)
+            }
+        }
+    }
+    
+    
+    @IBAction func buttons(_ sender: UIButton) {
+        if (sender.tag == 11) {
+            display.text = "0"
+            tracker = 0
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
